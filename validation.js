45eccf33f86eadbc52a013b8c676fbe6d53b4449
ndongba <ndongba01@gmail.com>
2024-07-15 16:05:04 +0000

    validation et fonctionnalités d'ajout et affichage sur le tableau terminées

--- validation.js
+++ validation.js
@@ -16,12 +16,85 @@ document.getElementById('form').addEventListener('submit', function(event) {
         setTimeout(function() {
             errorMessage.style.display = 'none';
             form.style.display = 'block';
-        }, 3000);
+        }, 2000);
     } else {
         // Vous pouvez soumettre le formulaire ou faire d'autres actions ici
         alert('Formulaire soumis avec succès');
     }
 });
+
+  // Fonctionnalités ajouter et afficher les idées
+
+   const idees = [
+    {id : 1, libelle : "Mon nom", categorie : "Sport", status : "approuvee" , description : "Ma description"}
+    
+  ];
+
+  function getAllIdees() {
+
+    idees.forEach(element => {
+      document.querySelector("tbody").innerHTML =
+          
+        `
+            <tr>
+                <th scope="row">${element.id}</th>
+                <td>${element.libelle}</td>
+                <td>${element.categorie}</td>
+                <td>${element.description}</td>
+                 <td>Approuvee</td>
+                <td>
+                    <button class="btn btn-primary">Approuvée</button>
+                    <button class="btn btn-danger">Désapprouvée</button>
+                </td>
+            </tr>
+        `;
+    })
+  }
+
+  getAllIdees();
+
+  // Permet d'ajouter une idee
+  function addIdee() {
+    // Objet user vide
+    newIdee = { id: 0, libelle: "", categorie: "", description: "" };
+
+    //Calcul de l'id
+
+    id = idees[idees.length -1].id+1;
+
+    //Recuperation des données saisies
+
+    libelle = document.getElementById("libelle").value;
+    categorie = document.getElementById("categorie").value;
+    description = document.getElementById("description").value;
+
+    console.log(libelle);
+    console.log(categorie);
+    console.log(description);
+
+    // Maj de l'objet newIdee
+
+    newIdee.id = id;
+    newIdee.libelle = libelle;
+    newIdee.categorie = categorie;
+    newIdee.description = description;
+
+    // Ajout de l'utilisateur
+    const indexIdee = idees.findIndex(idee => idee.libelle === newIdee.libelle);
+
+    if (indexIdee != -1) {
+      showMessage()
+      
+    }
+
+    idees.push(newIdee);
+    document.querySelector("tbody").innerHTML = "";
+    getAllIdees();
+
+  }
+
+
+
 
 
 
